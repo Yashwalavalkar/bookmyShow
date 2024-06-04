0c76f340e7951063a385fb0e160b771054ef5f4c
yashnw <yashwalavalkar0077@gmail.com>
2024-06-04 22:00:21 +0530

    lat

--- backend/index.js
+++ backend/index.js
@@ -4,48 +4,49 @@ const PORT = 8000;
 const mongoDb = require("./db");
 const cors = require("cors");
 const Booking = require("./model/Booking");
+const router = express.Router();
 
 // Connect to MongoDB
-mongoDb();
-app.use(cors());
-// Get allowed origins from environment variables
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://wondrous-torte-e1cc88.netlify.app");
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-  ); 
-  next();
-});
+try {
+  mongoDb();
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+}
 
+// Cors configuration
+const allowedOrigins = [process.env.ALLOWED_ORIGIN || "https://wondrous-torte-e1cc88.netlify.app"];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+}));
 
 // Parse JSON bodies
 app.use(express.json());
 
-app.get("/", (req, res) => {
+// Define routes
+router.get("/", (req, res) => {
   res.json({ message: "hello from backend" });
 });
 
-
-
-
-app.get("/api/booking", async (req, res) => {
+router.get("/api/booking", async (req, res) => {
   try {
-    const booking = await Booking.find();
-    const data = booking[booking.length - 1];
-    if (booking.length === 0) {
+    const bookings = await Booking.find();
+    if (bookings.length === 0) {
       return res.status(200).json({ message: "No booking details available." });
     }
-    res.status(200).json(data);
+    res.status(200).json(bookings);
   } catch (error) {
-    console.log(error);
+    console.error("Error fetching bookings:", error);
     res.status(500).json({ success: false, error: error.message });
   }
 });
 
-
-app.use("/api", require("./route/CreateBooking"));
-
+app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
