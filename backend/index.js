bab78cf9bceaf83eab484a7936a1ccf1eb61dc75
yashnw <yashwalavalkar0077@gmail.com>
2024-06-04 17:37:53 +0530

    data successfully commit

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const Booking = require("./model/Booking");
 
 // Connect to MongoDB
 mongoDb();
-
+app.use(cors());
 // Get allowed origins from environment variables
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://radiant-pasca-cf3c66.netlify.app");
